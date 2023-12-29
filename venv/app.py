e15a802b2135dfc37abc9d8e99bf036bde67bc82
Owen Hall <owen9hall@gmail.com>
2023-12-29 13:46:06 -0800

    SQL working db with flask

--- venv/app.py
+++ venv/app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+
+#configure application
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello World, from Flask!"
 
+#connect to SQL db
+def get_db():
+    path = "C:\\Users\\owenr\\Documents\\GitHub\\cs50FinalProj\\venv\\assignments.db"
+    return sqlite3.connect(path)
+
+
+@app.route("/")
+def index():
+
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT person_name FROM test WHERE person_id = ?", (1,)
+    )
+    name = cursor.fetchone()[0]
+    conn.close()
+    
+    return render_template("index.html", name=name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ venv/mysql.sql
@@ -0,0 +1,10 @@
+
+
+CREATE TABLE IF NOT EXISTS test(person_id INTEGER NOT NULL PRIMARY KEY, person_name TEXT NOT NULL);
+
+--INSERT INTO test(person_id, person_name)
+--VALUES (1, "Sam");
+
+SELECT * FROM test;
+
+--.schema
